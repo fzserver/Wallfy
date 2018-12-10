c5999171131b791706c7982785af8f15dd125e1f
FrazileDevelopers <frazile.com@gmail.com>
2018-12-11 00:04:01 +0530

    wallfy onTap() fuction added

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,9 @@ Widget wallurls(BuildContext context, List<DocumentSnapshot> wallList) =>
         elevation: 8.0,
         borderRadius: BorderRadius.all(Radius.circular(8.0)),
         child: InkWell(
+          onTap: () => Navigator.push(context, MaterialPageRoute(
+            builder: (context) => FullScreenImage(wallList[index].data['url'])
+          )),
           child: Hero(
             tag: wallList[index].data['url'],
             child: FadeInImage(
@@ -94,3 +97,53 @@ Widget wallurls(BuildContext context, List<DocumentSnapshot> wallList) =>
     crossAxisSpacing: 8.0,
   )
   : Center(child: CircularProgressIndicator(),);
+
+
+  class FullScreenImage extends StatelessWidget {
+    final String imgPath;
+    FullScreenImage(this.imgPath);
+
+    final LinearGradient backgroundGradient = LinearGradient(
+      colors: [Color(0x10000000), Color(0x30000000)],
+      begin: Alignment.topLeft, 
+      end: Alignment.bottomRight
+    );
+
+    @override
+    Widget build(BuildContext context)
+    =>  Scaffold(
+        body: SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(gradient: backgroundGradient),
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Hero(
+                    tag: imgPath,
+                    child: Image.network(imgPath),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      AppBar(
+                        elevation: 0.0,
+                        backgroundColor: Colors.transparent,
+                        leading: IconButton(
+                          icon: Icon(Icons.close, color: Colors.black,),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
