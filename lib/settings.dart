570226b43dbb3ae90ae63988c331b9d7acf5d33f
FrazileDevelopers <frazile.com@gmail.com>
2018-12-13 13:38:11 +0530

    wallfy

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:launch_review/launch_review.dart';
+import 'package:package_info/package_info.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+
+  @override
+  SettingsState createState() =>
+    SettingsState();
+}
+
+class SettingsState extends State<Settings> {
+  
+  String version = "";
+  
+  @override
+    void initState() {
+      super.initState();
+      getPackageInfo();
+    }
+
+  getPackageInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+          version = packageInfo.version;
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: settingsBody()
+      body: settingsBody(version)
     );
   }
 }
 
-ListView settingsBody() =>
+ListView settingsBody(String version) =>
   ListView(
     children: <Widget>[
       ListTile(
         leading: Icon(Icons.info),
         title: Text('Version', style: TextStyle(color: Colors.pink),),
-        subtitle: Text('1.0.0')
+        subtitle: Text('$version')
       ),
       ListTile(
         leading: Icon(Icons.shop),
